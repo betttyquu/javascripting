570d64530a8d80a5bb42a9eb0190838b0f492fac
betttyquu <absheng947@hotmail.com>
2018-09-07 00:45:58 +0000

    test

--- ice.js
+++ ice.js
@@ -1,7 +1,7 @@
 var ice = ["chocolate", "vailla", "cookies and cream", "rocky road", "starwberry"];
 // ice.push('root beer');
 // console.log(ice)
-var con = ['chocolate','blueberry', 'strawberry']
+var con = ['chocolate','blueberry']
 // console.log(con);
 // con.pop('blueberry');
 // console.log(con);
